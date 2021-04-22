d78e2b20f901bbb7962b7143282fad589a43a291
Jailton Junior <jailton.junior94@outlook.com>
2021-04-22 20:37:24 -0300

    Ajustes para obter outputs

--- Infrastructure/outputs.tf
+++ Infrastructure/outputs.tf
@@ -1,3 +1,3 @@
-# output "connection_azure_poc_blob" {
-#   value = azurerm_storage_account.azure_poc_blob.primary_blob_connection_string
-# }
+output "connection_azure_poc_blob" {
+  value = nonsensitive(azurerm_storage_account.azure_poc_blob.primary_blob_connection_string)
+}
--- readme.md
+++ readme.md
@@ -6,3 +6,7 @@ Projeto com objetivo de ajudar a fazer UPLOAD e DOWNLOAD de arquivos utilizando 
 
 1. Terraform para criar nosso Storage Account no Azure
 2. Projeto do tipo Web API (.net core 3.1)
+3. Para exibir outputs sensiveis 
+   ```
+    terraform output -json
+   ```
